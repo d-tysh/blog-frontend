ae1bea3592207216f2f3eedcbec5ccf9e1bd0676
d-tysh <39271488+d-tysh@users.noreply.github.com>
2024-10-05 12:24:30 +0300

    Commit

--- src/components/news/NewsSlider.tsx
+++ src/components/news/NewsSlider.tsx
@@ -1,25 +1,32 @@
 import { useSelector } from "react-redux";
-import { selectLastNews } from "../../redux/news/selectors";
+import { selectIsLoading, selectLastNews } from "../../redux/news/selectors";
 import { Slider } from "../Slider";
 import { SwiperSlide } from "swiper/react";
 import { NavLink } from "react-router-dom";
+import { Loader } from "../Loader";
 
 export const NewsSlider = () => {
     const lastNews = useSelector(selectLastNews);
+    const isLoading = useSelector(selectIsLoading);
 
     return (
-        lastNews.length &&
-        <Slider>
+        <>
+            {isLoading && !lastNews.length && <Loader />}
             {
-                lastNews.map(item => <SwiperSlide key={item._id}>
-                    <div className="flex justify-center h-[300px] sm:px-16">
-                        <NavLink
-                            to={`/news/${item._id}`}
-                            className='flex justify-center items-center bg-lime-200 w-full h-full p-4 sm:p-8'
-                        >{item.title}</NavLink>
-                    </div>
-                </SwiperSlide>)
+                !isLoading && lastNews.length > 0 &&
+                <Slider>
+                    {
+                        lastNews.map(item => <SwiperSlide key={item._id}>
+                            <div className="flex justify-center h-[300px] sm:px-16">
+                                <NavLink
+                                    to={`/news/${item._id}`}
+                                    className='flex justify-center items-center bg-lime-200 w-full h-full p-4 sm:p-8'
+                                >{item.title}</NavLink>
+                            </div>
+                        </SwiperSlide>)
+                    }
+                </Slider>
             }
-        </Slider>
+        </>
     );
 };
