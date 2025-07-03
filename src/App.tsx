d6fdf4f4d4b9076f44cf7be57dfcf66bedf429ff
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-07-03 13:38:07 +0300

    Add component AppRoutes.tsx and move all routes from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css';
-import { Route, Routes } from 'react-router-dom';
-import { lazy, useEffect } from 'react';
+import { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { useAppDispatch } from './hooks';
 import { fetchCurrentUser } from './redux/auth/actions';
 
-import { Layout } from './components/Layout';
-import { RestrictedRoute } from './components/RestrictedRoute';
-import { PrivateRoute } from './components/PrivateRoute';
-import { UserInfoPage } from './pages/UserInfoPage';
-import { AddUserPage } from './pages/AddUserPage';
 import { OnlineStatusManager } from './components/OnlineStatusManager';
-
-const HomePage = lazy(() => import('./pages/HomePage'));
-const NewsPage = lazy(() => import('./pages/NewsPage'));
-const LoginPage = lazy(() => import('./pages/LoginPage'));
-const RegisterPage = lazy(() => import('./pages/RegisterPage'));
-const OneNewsPage = lazy(() => import('./pages/OneNewsPage'));
-const AddNewsPage = lazy(() => import('./pages/AddNewsPage'));
-const UpdateNewsPage = lazy(() => import('./pages/UpdateNewsPage'));
-const AllUsersPage = lazy(() => import('./pages/AllUsersPage'));
-const PageNotFound = lazy(() => import('./pages/PageNotFound'));
+import { AppRoutes } from './AppRoutes';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -33,36 +18,7 @@ function App() {
   return (
     <>
       <OnlineStatusManager />
-      <Routes>
-        <Route path='/' element={<Layout />} >
-          <Route index element={<HomePage />} />
-          <Route 
-            path='/login' 
-            element={ <RestrictedRoute redirectTo='/' component={<LoginPage />} /> } 
-          />
-          <Route path='/register' 
-            element={ <RestrictedRoute redirectTo='/' component={<RegisterPage />} />} 
-          />
-          <Route path='/news' element={<NewsPage />} />
-          <Route path='/add-news' 
-            element={<PrivateRoute redirectTo='/login' component={<AddNewsPage />} />} 
-          />
-          <Route path='/news/:newsUrl' element={<OneNewsPage />} />
-          <Route path='/news/update/:newsId' element={
-            <PrivateRoute redirectTo='/login' component={<UpdateNewsPage />} />
-          } />
-          <Route path='/users' element={
-            <PrivateRoute redirectTo='/' component={<AllUsersPage />} />
-          } />
-          <Route path='/users/:userId' element={
-            <PrivateRoute redirectTo='/' component={<UserInfoPage />} />
-          } />
-          <Route path='/users/add' element={
-            <PrivateRoute redirectTo='/' component={<AddUserPage />} />
-          } />
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-      </Routes>
+      <AppRoutes />
       <ToastContainer />
     </>
   )
